b8d4d0cb96131c1c7c9d5731bac59526e457cf71
Ali Fawzy <ali.fawzy.ali1997@gmail.com>
2025-10-22 11:58:13 +0300

    sdsdsdsdsdsdsdgffghhhhgh

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,9 +2,8 @@ class Constants {
   static const String isUserLoggedIn = 'isLoggedIn';
   static const String kAccessToken = "accessToken";
   static const String kRefreshToken = "refreshToken";
-  static const String baseUrl =
-      // "http://api_reqaba.pm/api/";
-      'https://pmis-api-st.digitalegypt.gov.eg/api/';
+  static const String baseUrl = "http://api_reqaba.pm/api/";
+  //'https://pmis-api-st.digitalegypt.gov.eg/api/';
   //'http://pmis.pm/api/';
   static const String authUrl = "Oauth/login";
   static const String dropDownsUrl =
